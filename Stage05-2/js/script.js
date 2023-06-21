d36fb01555dee5c5645d4e7203646d0edc3b3aa7
KaynanSantos <kaynan_brito@hotmail.com>
2023-06-21 16:37:43 -0300

    Add files via upload

---
+++ Stage05-2/js/script.js
@@ -0,0 +1,39 @@
+import { Modal } from './modal.js'
+
+// variáveis - variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+form.onsubmit = event => {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if (showAlertError) {
+        console.log('mostrar o alerta de erro')
+        return;
+    }
+
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
+}
+
+function notANumber(value) {
+    return isNaN(value) || value == ""
+}
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
+}
+
+inputWeight.oninput = ()=> AlertError.close()
+inputHeight.oninput = ()=> AlertError.close()
